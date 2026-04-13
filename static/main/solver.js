0742fc66c649eb28bdbb4653a362132e51515d84
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-04-13 16:08:50 +0300

    solver.js: Build constraints for unsolved cells.

--- static/main/solver.js
+++ static/main/solver.js
@@ -2,6 +2,18 @@ export default class Solver {
     constructor(api) {
         this.api = api;
         this.difficulty = this.api.getDifficulty();
+
+        /*
+            * Solved cells are cells that are open, and all its safe neighbours
+            * are opened as well. It's a cell where the number of unopened
+            * neighbours equals the number displayed on the cell (the number
+            * of mines around the cell). The constraints produced by these cells
+            * are satisfied.
+            *
+            * They are tracked in the array below to know which cells to avoid
+            * when building new constraints.
+            */
+        this.solvedCells = [];
     }
 
     /*
@@ -9,6 +21,9 @@ export default class Solver {
         */
     solve() {
         this.openRandomCell();
+
+        const constraints = this.buildConstraints();
+        console.log(constraints);
     }
 
     /*
@@ -17,5 +32,63 @@ export default class Solver {
     openRandomCell() {
         const index = Math.floor(Math.random() * this.difficulty.cellCount);
         this.api.openCell(index);
+    }
+
+    /*
+        * Build constraints for all unsolved cells. Unsolved cells are cell
+        * that have more covered neighbours than the number of mines around
+        * them, meaning that some of its neighbours are covered and need to
+        * be opened.
+        *
+        * A constraints contains an array of variables, and the mine count.
+        * Variables are covered unflagged cells. The mine count is the number
+        * of mines around the cell. Example constraint:
+        *   { variables: [0, 2, 8], mineCount: 2 }
+        *
+        * This function returns an array of constraints.
+        */
+    buildConstraints() {
+        const openCells = this.api.getOpenCells();
+        const flaggedCells = this.api.getFlaggedCells();
+
+        // Remove solved cells.
+        const unsolvedCells = [];
+        for (const index of openCells) {
+            if (this.solvedCells.includes(index)) continue;
+            unsolvedCells.push(index);
+        }
+
+        // Build constraints.
+        const constraints = [];
+        for (const cellIndex of unsolvedCells) {
+            const variables = [];
+            let mineCount = this.api.getCellMineCount(cellIndex);
+
+            const cellNeighbours = this.api.getCellNeighbours(cellIndex);
+            for (const neighbourIndex of cellNeighbours) {
+                // Reduce required mines for every flagged neighbour.
+                const flagged = this.api.isFlagged(neighbourIndex);
+                if (flagged) mineCount--;
+
+                // Add unflagged closed neighbours to variables.
+                const state = this.api.getCellState(neighbourIndex);
+                if (!flagged && state === "closed") {
+                    variables.push(neighbourIndex);
+                }
+            }
+
+            // Keep track of solved cells and skip them.
+            // Solved cells have a no variables and no mineCount.
+            if (variables.length === 0 && mineCount === 0) {
+                this.solvedCells.push(cellIndex);
+                continue;
+            }
+
+            // Final constraint.
+            const constraint = { "variables": variables, "mineCount": mineCount }
+            constraints.push(constraint);
+        }
+
+        return constraints;
     }
 }
