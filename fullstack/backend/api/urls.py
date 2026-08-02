51c4efb9f0327fc7d7ccef77902a7811d522fea7
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-08-02 20:32:44 +0300

    Remove the entire 'fullstack' directory.
    I want this to be a purely frontend app.

--- fullstack/backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import generate_new_board
-
-urlpatterns = [
-    path("new/", generate_new_board),
-]
--- fullstack/backend/api/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-# Create your views here.
-def generate_new_board(request):
-    return HttpResponse("It works.")
